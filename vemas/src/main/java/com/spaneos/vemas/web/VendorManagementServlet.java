02ea91d84f0e883557e4e6fb3844259332bff893
Srikanth <Srikanth@192.168.1.214>
2015-05-25 15:06:29 +0530

    small change

--- vemas/src/main/java/com/spaneos/vemas/web/VendorManagementServlet.java
+++ vemas/src/main/java/com/spaneos/vemas/web/VendorManagementServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.jsp.PageContext;
 
 import com.spaneos.vemas.pojo.Bank;
 import com.spaneos.vemas.pojo.Billes;
